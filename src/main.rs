8b50adca73453426c080c19cab233efb2aa6a5d4
Christian Stampfl <christian.stampfl@tum.de>
2022-09-12 22:24:37 +0200

    retry notification if timeout occurs

--- src/main.rs
+++ src/main.rs
@@ -27,6 +27,8 @@ pub struct ApiConfig {
 struct TelegramConfig {
     token: String,
     chatid: i64,
+    retries: u8,
+    interval: u8,
 }
 #[derive(Debug, Deserialize)]
 struct Config {
@@ -42,66 +44,126 @@ fn read_config(config_fp: String) -> Result<Config, anyhow::Error> {
     Ok(c)
 }
 
-async fn send_notification(chat_id: ChatId, bot: &AutoSend<Bot>, message: String) {
+async fn send_notification(chat_id: ChatId, bot: &AutoSend<Bot>, message: &String) -> bool {
     match bot.send_message(chat_id, message).await {
-        Result::Ok(_) => (),
-        // Debug error since it may not interrupt the logic (may be time out e.g.)
-        Err(e) => {
-            dbg!(e);
-        }
+        Result::Ok(_) => false,
+        Err(e) => match e {
+            teloxide::RequestError::Network(err) => {
+                if err.is_timeout() {
+                    true
+                } else {
+                    dbg!(err);
+                    false
+                }
+            }
+            // Debug error since it may not interrupt the logic
+            _ => {
+                dbg!(e);
+                false
+            }
+        },
     }
 }
 
+async fn send_notfication_until(
+    chat_id: ChatId,
+    bot: &AutoSend<Bot>,
+    retries: u8,
+    interval: u8,
+    message: String,
+) {
+    let mut tried = 0;
+    while tried < retries && send_notification(chat_id, bot, &message).await {
+        tried += 1;
+        dbg!("Retry {}", &tried);
+        let _ = time::sleep(time::Duration::from_secs(interval as u64));
+    }
+}
 
 async fn update(
-    url: &String,
+    config: &Config,
     login_request: &RpcRequest,
     update_request: &RpcRequest,
-    update_key: &String,
-    storage_file: &String,
     chat_id: ChatId,
     bot: &AutoSend<Bot>,
 ) {
-    match api::get_current_state(url, login_request, update_request, update_key).await {
+    match api::get_current_state(
+        &config.api.url,
+        login_request,
+        update_request,
+        &config.api.update_key,
+    )
+    .await
+    {
         Result::Ok(s) => {
-            if let Result::Ok(previous) = load_current(storage_file) {
+            if let Result::Ok(previous) = load_current(&config.storage) {
                 if previous.ip != s.ip {
-                    send_notification(
+                    send_notfication_until(
                         chat_id,
                         bot,
+                        config.telegram.retries,
+                        config.telegram.interval,
                         format!("IP changed from {} to {}", &previous.ip, &s.ip),
                     )
-                    .await
+                    .await;
                 }
 
                 for (k, v) in &s.queue {
                     let previous_progress = previous.queue.get(k);
                     if let Some(p) = previous_progress {
                         if v == &100.0 && p < &100.0 {
-                            send_notification(chat_id, bot, format!("Done with {}", &k)).await
+                            send_notfication_until(
+                                chat_id,
+                                bot,
+                                config.telegram.retries,
+                                config.telegram.interval,
+                                format!("Done with {}", &k),
+                            )
+                            .await;
                         }
                     }
-                    if let Result::Err(e) = save_current(storage_file, &s) {
+                    if let Result::Err(e) = save_current(&config.storage, &s) {
                         // Exit program bc initial storage file cannot be created => unable to do the logic
                         let msg = format!("Could not save current state {:?}", &e);
-                        send_notification(chat_id, bot, msg).await;
+                        send_notfication_until(
+                            chat_id,
+                            bot,
+                            config.telegram.retries,
+                            config.telegram.interval,
+                            msg,
+                        )
+                        .await;
                         panic!("Could not save current state {:?}", &e);
                     }
                 }
             } else {
-                match init_current(storage_file, &s) {
+                match init_current(&config.storage, &s) {
                     Result::Ok(_f) => (),
                     // Exit program bc initial storage file cannot be created => unable to do the logic
                     Err(e) => {
-                        send_notification(chat_id, bot, format!("Unable to create init storage file {:?}", &e)).await;
+                        send_notfication_until(
+                            chat_id,
+                            bot,
+                            config.telegram.retries,
+                            config.telegram.interval,
+                            format!("Unable to create init storage file {:?}", &e),
+                        )
+                        .await;
                         panic!("Unable to create init storage file {:?}", &e)
-                    },
+                    }
                 }
             }
         }
         Err(e) => {
             // Debug error since it may not interrupt the logic (may be time out e.g.)
-            send_notification(chat_id, bot, format!("Unabel to get current state {:?}", &e)).await;
+            send_notfication_until(
+                chat_id,
+                bot,
+                config.telegram.retries,
+                config.telegram.interval,
+                format!("Unabel to get current state {:?}", &e),
+            )
+            .await;
             dbg!(e);
         }
     }
@@ -114,19 +176,19 @@ async fn main() -> Result<(), JoinError> {
         println!("Usage: api-telegram <path/to/config>")
     }
 
-    let config = match read_config(args.pop().unwrap()) {
+    let config = Arc::new(match read_config(args.pop().unwrap()) {
         Result::Ok(c) => c,
         Err(e) => panic!("Unable to read config file! : {:?}", e),
-    };
+    });
 
     let login_request = Arc::new(RpcRequest {
         id: 1,
-        method: config.api.login_method,
-        params: vec![Value::String(config.api.password)],
+        method: config.api.login_method.clone(),
+        params: vec![Value::String(config.api.password.clone())],
     });
     let update_request = Arc::new(RpcRequest {
         id: 1,
-        method: config.api.update_method,
+        method: config.api.update_method.clone(),
         params: vec![
             Value::Array(vec![
                 Value::String("name".to_string()),
@@ -135,19 +197,15 @@ async fn main() -> Result<(), JoinError> {
             Value::Object(Map::new()),
         ],
     });
-    let url = Arc::new(config.api.url);
-    let update_key = Arc::new(config.api.update_key);
     let bot = Bot::new(&config.telegram.token).auto_send();
     let task = task::spawn(async move {
         let mut interval = time::interval(Duration::from_secs(config.interval));
         loop {
             interval.tick().await;
             update(
-                url.clone().as_ref(),
+                config.clone().as_ref(),
                 login_request.clone().as_ref(),
                 update_request.clone().as_ref(),
-                update_key.clone().as_ref(),
-                &config.storage,
                 ChatId(config.telegram.chatid),
                 &bot,
             )
